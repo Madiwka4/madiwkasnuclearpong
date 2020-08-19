94fc1207c92100dff32888e41cce531a2a67d30e
Madiwka <mainbox@madi-wka.club>
2020-08-19 11:36:59 +0600

    smoooth

--- main.lua
+++ main.lua
@@ -808,7 +808,7 @@ function nettest(dt)
         end
 
         
-        if (ball[1].x > VIRTUAL_WIDTH/2) then 
+        if (ball[1].x > VIRTUAL_WIDTH/2) and gameState ~= "play" then 
             die = tonumber(p[2])
             lastSentKeyClient, 
             ball[1].dy, 
@@ -893,7 +893,7 @@ function clienttest(dt)
             end 
             for i = 1, maxBalls do 
             local die = tonumber(p[2])
-            if (ball[i].x <= VIRTUAL_WIDTH/2) then 
+            if (ball[i].x <= VIRTUAL_WIDTH/2) or gameState ~= "play" then 
                 lastSentKeyClient, ball[i].dy, player1.y, player1score, player2score, player1nukescore, player2nukescore, ball[i].x, ball[i].y, gameState, ball[i].dx, ballSpeed, paddle_SPEED = p[1], die, tonumber(p[4]), tonumber(p[5]), tonumber(p[6]), tonumber(p[7]), tonumber(p[8]), tonumber(p[9]), tonumber(p[10]), p[11], tonumber(p[12]), tonumber(p[13]), tonumber(p[14])
             else 
                 lastSentKeyClient = p[1] 
