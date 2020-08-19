e731f9f7eae2dc71f6463c49c284d1acf24f9c86
Madiwka <mainbox@madi-wka.club>
2020-08-19 12:13:41 +0600

    Changed ball serving

--- main.lua
+++ main.lua
@@ -811,6 +811,7 @@ function nettest(dt)
 
         
         if (ball[1].x > VIRTUAL_WIDTH/2) then 
+            if tonumber(p[9]) > VIRTUAL_WIDTH/2 then
             die = tonumber(p[2])
             lastSentKeyClient, 
             ball[1].dy, 
@@ -825,6 +826,7 @@ function nettest(dt)
             ball[1].dx, 
             ballSpeed, 
             paddle_SPEED = p[1], die, tonumber(p[4]), tonumber(p[5]), tonumber(p[6]), tonumber(p[7]), tonumber(p[8]), tonumber(p[9]), tonumber(p[10]), p[11], tonumber(p[12]), tonumber(p[13]), tonumber(p[14])
+            end
         else  
             lastSentKeyClient = p[1]
             player2.y = tonumber(p[4])
@@ -895,8 +897,10 @@ function clienttest(dt)
             end 
             for i = 1, maxBalls do 
             local die = tonumber(p[2])
-            if (ball[i].x <= VIRTUAL_WIDTH/2) then 
+            if (ball[i].x <= VIRTUAL_WIDTH/2) then
+                if tonumber(p[9]) <= VIRTUAL_WIDTH/2 then 
                 lastSentKeyClient, ball[i].dy, player1.y, player1score, player2score, player1nukescore, player2nukescore, ball[i].x, ball[i].y, gameState, ball[i].dx, ballSpeed, paddle_SPEED = p[1], die, tonumber(p[4]), tonumber(p[5]), tonumber(p[6]), tonumber(p[7]), tonumber(p[8]), tonumber(p[9]), tonumber(p[10]), p[11], tonumber(p[12]), tonumber(p[13]), tonumber(p[14])
+                end
             else 
                 lastSentKeyClient = p[1] 
                 player1.y = tonumber(p[4])
